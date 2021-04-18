563096a13bcc6a84b3a0ffe36f16885f3fdcbccf
Stephanie Reinders <reinders.stephanie@gmail.com>
2021-04-18 12:02:59 -0500

    change layout to sidebar

--- ui.R
+++ ui.R
@@ -1,13 +1,24 @@
 library(shiny)
 shinyUI(
   fluidPage(
-    tags$b("Simple counter using reactiveValues() - An example"),
-    br(),
-    actionButton("add1", "+ 1"),
-    actionButton("sub1", "- 1"),
-    actionButton("reset", "set to 0"),
-    br(),
-    textOutput("count")
+    #--- new
+    # Title
+    titlePanel("Simple counter using reactiveValues() - An example"),
+    
+    sidebarLayout(      
+      
+      # Side panel
+      sidebarPanel(
+        actionButton("add1", "+ 1"),
+        actionButton("sub1", "- 1"),
+        actionButton("reset", "set to 0"),
+        br()
+      ),
+      
+      # Main panel
+      mainPanel(
+        textOutput("count")
+      )
+    )
   )
-  
 )
