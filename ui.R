099389103f110afe5ff74da1160c792d01becfc4
Stephanie Reinders <reinders.stephanie@gmail.com>
2021-04-20 12:56:02 -0500

    change layout

--- ui.R
+++ ui.R
@@ -1,45 +1,44 @@
 library(shiny)
 shinyUI(
   fluidPage(
-    #--- new
-    # Title
     titlePanel("Island Hopping Simulation"),
     
-    sidebarLayout(      
+    fluidRow(
+      column(3,
       
-      # Side panel
-      sidebarPanel(
-        
+      tableOutput("df"),
+      textOutput("proposed_island"),
+      )
+    ),
+    
+    
+    fluidRow(      
+      column(3,
         # Parameters
         numericInput(inputId = "num_islands", 
                      label = "Number of islands:",
                      min = 3,
                      max = 20,
                      value = 5),
-        
         numericInput(inputId = "max_pop", 
                      label = "Maximum population of each island:",
                      min = 5,
                      max = 50,
                      value = 10),
-        
         numericInput(inputId = "starting_island", 
                      label = "Starting island:",
                      min = 1,
                      max = 20,
-                     value = 3),
+                     value = 3)
+        ),
+      column(4,
         
         # Buttons
         actionButton("startButton", "Start your trip"),
+        actionButton("considerButton", "Consider visiting another island"),
         actionButton("visitButton", "Visit another island?"),
         br()
-      ),
-      
-      # Main panel
-      mainPanel(
-        tableOutput("df"),
-        textOutput("proposed_island")
+        )
       )
     )
   )
-)
