5900d607570bae43030cc627f9c5eee8bbc7c089
Stephanie Reinders <reinders.stephanie@gmail.com>
2025-08-07 15:46:31 -0500

    fixed bug so that current island name is correctly updated

--- R/visit_island.R
+++ R/visit_island.R
@@ -1,25 +1,28 @@
 library(stats)
 
 visit_island <- function(current_island, proposed_island, num_islands, df){
-  # Propose new island
-  if(current_island == 1){
-    proposed_island <- 2
-  } else if (current_island == num_islands){
-    proposed_island <- num_islands - 1
-  } else {
-    # Randomly choose whether to consider a left or right move: 0=left and 1=right.
-    direction = sample.int(n=2, size=1)
-    if (direction==1){
-      proposed_island <- current_island + 1
-    } else {
-      proposed_island <- current_island - 1
-    }
-  }
   
-  #Chose whether to move to proposed island
-  island_pop <- df$island_pop
-  pi_move = min(island_pop[proposed_island]/island_pop[current_island], 1)
-  if (island_pop[proposed_island] > island_pop[current_island]){
+  # Randomly choose whether to consider a left or right move: 0=left and 1=right.
+  direction = sample.int(n=2, size=1)
+  
+  # Propose new island
+  proposed_island <- dplyr::case_when(
+    current_island == 1 & direction == 1 ~ num_islands,
+    current_island == num_islands & direction == 2 ~ 1,
+    direction == 2 ~ current_island + 1,
+    direction == 1 ~ current_island - 1
+  )
+
+  # Chose whether to move to proposed island ---- Rule 1. If the proposed island
+  # has a larger population that the current island, move to the proposed
+  # island. Rule 2. Randomly select a number u from the uniform distribution on
+  # [0,1]. Calculate the probability of a move pi_move as 1 if the population of
+  # the proposed island is larger than the current island's population, and the
+  # population of the proposed island divided by the current island's population
+  # otherwise. If u is less than or equal to pi_move, then move to the proposed
+  # island, otherwise stay on the current island.
+  pi_move = min(df$island_pop[proposed_island]/df$island_pop[current_island], 1)
+  if (df$island_pop[proposed_island] > df$island_pop[current_island]){
     visit_yn <- 'y'
   } else {
     u = runif(n=1)
