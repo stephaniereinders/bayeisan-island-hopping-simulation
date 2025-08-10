95a50fa6c8e08b997f826c9575461d2de5347d52
Stephanie Reinders <reinders.stephanie@gmail.com>
2025-08-10 13:31:43 -0500

    add help text for the buttons

--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@ shinyUI(
     
     titlePanel("Island Hopping Simulation"),
     
-    p("Simulate island hopping using Bayesian statistics and the Metropolis-Hastings algorithm. This interactive simulation demonstrates how populations influence movement decisions in a chain of islands."),
+    p("This interactive simulation demonstrates the Metropolis-Hastings algorithm using a charity fundraising scenario. You're traveling between a chain of islands to raise money for charity. To maximize your fundraising success, you need to visit larger islands (with bigger populations) more often than smaller ones. Specifically, the frequency of your visits to each island should be proportional to that island's population size."),
     
     sidebarLayout(
       sidebarPanel(width = 3,
@@ -17,6 +17,7 @@ shinyUI(
         # Parameters
         h4("Choose Parameters for the Simulation"),
         fluidRow(
+          column(width = 12, p("Choose the number of islands (between 3 and 20), the starting island, and the maximum possible population for each island. The application will randomly select populations between 1 and the maximum population with replacement.")),
           column(width = 6,
                  numericInput(inputId = "num_islands", 
                               label = "Number of islands",
@@ -42,6 +43,8 @@ shinyUI(
         # Buttons
         h4("Simulations"),
         fluidRow(
+          column(width = 12, 
+                 p("Click 'Start trip' to begin the simulation. Click 'Visit island' decide whether to move to another island. The data frame below the islands plot tracks the number of visits to each island.")),
           column(width = 6,
                  actionButton("startButton", "Start trip")
             
@@ -52,6 +55,8 @@ shinyUI(
         ),
         
         br(),
+        fluidRow(column(width = 12, 
+                        p("Select the desired number of visits on the slider and click 'Simulate n visits' to simulate multiple visits all at once."))),
         
         sliderInput(inputId="n",
                     label="Number of visits",
